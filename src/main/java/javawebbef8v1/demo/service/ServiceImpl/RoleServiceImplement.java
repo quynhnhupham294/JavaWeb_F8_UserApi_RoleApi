d524e25da85f9a9fc4ba2b1a8cb103ac16f4bdbe
quynhnhupham294 <quynhpnps39894@gmail.com>
2024-01-23 22:19:34 +0700

    fix location file class impl -final

--- src/main/java/javawebbef8v1/demo/service/ServiceImpl/RoleServiceImplement.java
+++ src/main/java/javawebbef8v1/demo/service/ServiceImpl/RoleServiceImplement.java
@@ -4,6 +4,7 @@ import javawebbef8v1.demo.entities.Role;
 import javawebbef8v1.demo.mapper.RoleMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import javawebbef8v1.demo.service.RoleService;
 
 import java.util.List;
 
